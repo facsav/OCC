ae0a0c1fceefd9153ee77190b7de1633157cdcea
facSav <facu.savio@gmail.com>
2024-05-31 10:54:30 -0300

    cambios

--- src/components/SobreNosotros.jsx
+++ src/components/SobreNosotros.jsx
@@ -237,7 +237,6 @@ useEffect(() => {
                                 <div class="carousel-item">
                                     <img src={assets.construccionCasas2} height='300px' alt='Imagen de Miami' />
                                 </div>
-
                             </div>
 
                         </div>
