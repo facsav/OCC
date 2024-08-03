a51181f371cce649b57c8d4166dc6f1991649422
facSav <facu.savio@gmail.com>
2024-08-03 19:43:48 -0300

    reinstalacion de react-snap

--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
 
-// if (rootElement.hasChildNodes()) {
-//   ReactDOM.hydrate(<App />, rootElement);
-// } else {
-//   ReactDOM.render(<App />, rootElement);
-// }
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<App />, rootElement);
+} else {
+  ReactDOM.render(<App />, rootElement);
+}
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
