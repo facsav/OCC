f58c1a3af7d5be5a5c7b6aa735d7e0c0686e4b92
MBLukrats <lucasmbuzzoni24@gmail.com>
2024-10-10 17:52:36 -0300

    Correccion de textos de trabajos

--- src/components/MasTrabajosNew.jsx
+++ src/components/MasTrabajosNew.jsx
@@ -29,18 +29,23 @@ export function MasTrabajosNew() {
                         <Modal modal={8} img={data.pools[7].portada} titulo={data.pools[7].name} desc={data.pools[7].desc_corta} imgModal={data.pools[7].modal } año={data.pools[7].año} mes={data.pools[7].mes} />  
                         <Modal modal={12} img={data.pools[11].portada} titulo={data.pools[11].name} desc={data.pools[11].desc_corta} imgModal={data.pools[11].modal} año={data.pools[11].año} mes={data.pools[11].mes}/>  
                         <Modal modal={14} img={data.pools[13].portada} titulo={data.pools[13].name} desc={data.pools[13].desc_corta} imgModal={data.pools[13].modal} año={data.pools[13].año} mes={data.pools[13].mes}/>  
+                        
                         <Modal modal={1} img={data.pools[0].portada} titulo={data.pools[0].name} desc={data.pools[0].desc_corta} imgModal={data.pools[0].modal} año={data.pools[0].año} mes={data.pools[0].mes}/>  
                         <Modal modal={2} img={data.pools[1].portada} titulo={data.pools[1].name} desc={data.pools[1].desc_corta} imgModal={data.pools[1].modal} año={data.pools[1].año} mes={data.pools[1].mes}/>  
                         <Modal modal={3} img={data.pools[2].portada} titulo={data.pools[2].name} desc={data.pools[2].desc_corta} imgModal={data.pools[2].modal} año={data.pools[2].año} mes={data.pools[2].mes}/>  
+                        
                         <Modal modal={4} img={data.pools[3].portada} titulo={data.pools[3].name} desc={data.pools[3].desc_corta} imgModal={data.pools[3].modal} año={data.pools[3].año} mes={data.pools[3].mes}/>  
                         <Modal modal={5} img={data.pools[4].portada} titulo={data.pools[4].name} desc={data.pools[4].desc_corta} imgModal={data.pools[4].modal} año={data.pools[4].año} mes={data.pools[4].mes}/>  
                         <Modal modal={6} img={data.pools[5].portada} titulo={data.pools[5].name} desc={data.pools[5].desc_corta} imgModal={data.pools[5].modal} año={data.pools[5].año} mes={data.pools[5].mes}/>  
+                        
                         <Modal modal={7} img={data.pools[6].portada} titulo={data.pools[6].name} desc={data.pools[6].desc_corta} imgModal={data.pools[6].modal} año={data.pools[6].año} mes={data.pools[6].mes}/>  
                         <Modal modal={11} img={data.pools[10].portada} titulo={data.pools[10].name} desc={data.pools[10].desc_corta} imgModal={data.pools[10].modal} año={data.pools[10].año} mes={data.pools[10].mes}/>  
                         <Modal modal={9} img={data.pools[8].portada} titulo={data.pools[8].name} desc={data.pools[8].desc_corta} imgModal={data.pools[8].modal} año={data.pools[8].año} mes={data.pools[8].mes}/>  
+                        
                         <Modal modal={10} img={data.pools[9].portada} titulo={data.pools[9].name} desc={data.pools[9].desc_corta} imgModal={data.pools[9].modal} año={data.pools[9].año} mes={data.pools[9].mes}/>  
                         <Modal modal={13} img={data.pools[12].portada} titulo={data.pools[12].name} desc={data.pools[12].desc_corta} imgModal={data.pools[12].modal} año={data.pools[12].año} mes={data.pools[12].mes}/>  
                         <Modal modal={15} img={data.pools[14].portada} titulo={data.pools[14].name} desc={data.pools[14].desc_corta} imgModal={data.pools[14].modal} año={data.pools[14].año} mes={data.pools[14].mes}/>  
+                        
                         <Modal modal={16} img={data.pools[15].portada} titulo={data.pools[15].name} desc={data.pools[15].desc_corta} imgModal={data.pools[15].modal} año={data.pools[15].año} mes={data.pools[15].mes}/>  
 
                     </div>
@@ -72,9 +77,11 @@ export function MasTrabajosNew() {
                         <Modal modal={31} img={data.pisos[5].cara} titulo={data.pisos[5].name} desc={data.pisos[5].desc_corta} imgModal={data.pisos[5].portada}  año={data.pisos[5].año} mes={data.pisos[5].mes}/>
                         <Modal modal={32} img={data.pisos[6].cara} titulo={data.pisos[6].name} desc={data.pisos[6].desc_corta} imgModal={data.pisos[6].portada}  año={data.pisos[6].año} mes={data.pisos[6].mes}/>
                         <Modal modal={33} img={data.pisos[7].cara} titulo={data.pisos[7].name} desc={data.pisos[7].desc_corta} imgModal={data.pisos[7].portada}  año={data.pisos[7].año} mes={data.pisos[7].mes}/>
+                        
                         <Modal modal={29} img={data.pisos[3].cara} titulo={data.pisos[3].name} desc={data.pisos[3].desc_corta} imgModal={data.pisos[3].portada}  año={data.pisos[3].año} mes={data.pisos[3].mes}/>
                         <Modal modal={30} img={data.pisos[4].cara} titulo={data.pisos[4].name} desc={data.pisos[4].desc_corta} imgModal={data.pisos[4].portada}  año={data.pisos[4].año} mes={data.pisos[4].mes}/>
                         <Modal modal={26} img={data.pisos[0].cara} titulo={data.pisos[0].name} desc={data.pisos[0].desc_corta} imgModal={data.pisos[0].portada}  año={data.pisos[0].año} mes={data.pisos[0].mes}/>
+                        
                         <Modal modal={28} img={data.pisos[2].cara} titulo={data.pisos[2].name} desc={data.pisos[2].desc_corta} imgModal={data.pisos[2].portada}  año={data.pisos[2].año} mes={data.pisos[2].mes}/>
                         <Modal modal={27} img={data.pisos[1].cara} titulo={data.pisos[1].name} desc={data.pisos[1].desc_corta} imgModal={data.pisos[1].portada}  año={data.pisos[1].año} mes={data.pisos[1].mes}/>
                         
